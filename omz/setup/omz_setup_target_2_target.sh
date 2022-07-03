6510f55e3bc4de25e7a89f075c0a7300fba2a5c2
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-03 22:31:17 +0900

    Update omz_setup_target_2_target.sh

--- omz/setup/omz_setup_target_2_target.sh
+++ omz/setup/omz_setup_target_2_target.sh
@@ -5,3 +5,6 @@ pip install --no-index --find-links=./pip -U pip
 pip install --no-index --find-links=./pip openvino==2022.1.0
 pip install --no-index --find-links=./pip opencv-python
 pip install --no-index --find-links=./pip -r pip/requirements.txt
+pip install --no-index --find-links=./pip pip/common/python
+
+
