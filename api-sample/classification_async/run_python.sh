04d7502e11b2213374e9508013489da06b5ddbdc
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-04 00:04:01 +0900

    Update run_python.sh

--- api-sample/classification_async/run_python.sh
+++ api-sample/classification_async/run_python.sh
@@ -1,3 +1,3 @@
 cd ~/handson ; . openvino/bin/activate
-python samples/python/hello_classification/classification_sample_async.py models/public/googlenet-v1/FP32/googlenet-v1.xml data/dog.bmp CPU
+python samples/python/classification_sample_async/classification_sample_async.py models/public/googlenet-v1/FP32/googlenet-v1.xml data/dog.bmp CPU
 
