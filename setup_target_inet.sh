645bd8fbdc77f27b002dc203709daec234ed22da
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-03 23:15:57 +0900

    Delete setup_target_inet.sh

--- setup_target_inet.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/handson/data; cd ~/handson
-python3 -m venv openvino
-. openvino/bin/activate
-python -m pip install --upgrade pip
-pip install openvino==2022.1.0
-pip install opencv-python
-
