98f4a5fe415c72105fcb3aef16405acfdf58f6eb
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-03 23:15:09 +0900

    Delete setup_host_devtool.sh

--- setup_host_devtool.sh
+++
@@ -1,8 +0,0 @@
-cd ~/handson; mkdir ~/handson/data
-sudo apt install python3-venv
-python3 -m venv ~/handson/openvino
-. ~/handson/openvino/bin/activate
-python -m pip install --upgrade pip
-pip install openvino-dev[tensorflow2,onnx,pytorch]==2022.1
-mo -h
-
