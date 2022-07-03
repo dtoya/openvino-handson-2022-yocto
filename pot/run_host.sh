f43f1be8e0aa8128dc0b9a5fd0c79dec9aeeafca
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-04 00:19:52 +0900

    Update run_host.sh

--- pot/run_host.sh
+++ pot/run_host.sh
@@ -1,3 +1,4 @@
-cd ~/handson/pot
+cd ~/handson
 . openvino/bin/activate
+cd pot
 pot -q default -m models/public/yolo-v4-tf/FP16/yolo-v4-tf.xml -w models/public/yolo-v4-tf/FP16/yolo-v4-tf.bin --engine simplified --data-source dataset/val2017
