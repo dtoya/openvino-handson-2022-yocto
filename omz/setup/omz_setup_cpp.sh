62f8f112886190684a79f74f905530b0022be402
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-03 22:15:39 +0900

    Update omz_setup_cpp.sh

--- omz/setup/omz_setup_cpp.sh
+++ omz/setup/omz_setup_cpp.sh
@@ -1,6 +1,6 @@
 cd ~/handson
 sudo apt install openvino-opencv-2022.1.0
-. /opt/intel/openvino_2022/setupvars.sh
+chmod +x open_model_zoo/demos/build_demos.sh
 sudo apt install build-essential
-sh open_model_zoo/demos/build_demos.sh -b=./omz_demos_build
+open_model_zoo/demos/build_demos.sh -b=./omz_demos_build
 
