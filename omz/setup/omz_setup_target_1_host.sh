55fcfb2a912159cb1950d25be88eab3d54dfeeba
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-03 22:54:42 +0900

    Update omz_setup_target_1_host.sh

--- omz/setup/omz_setup_target_1_host.sh
+++ omz/setup/omz_setup_target_1_host.sh
@@ -2,7 +2,7 @@ cd ~/handson
 . venv/bin/activate
 pip download -d pip -r open_model_zoo/demos/requirements.txt
 pip download -d pip open_model_zoo/demos/common/python/
-rsync -av pip/ <user>@<ip>:~/handson/pip/
+rsync -av pip/ $TUSER@$TIP:~/handson/pip/
 scp open_model_zoo/demos/requirements.txt $TUSER@$TIP:~/handson/pip
 scp -r open_model_zoo/demos/common $TUSER@$TIP:~/handson/pip
 deactivate
