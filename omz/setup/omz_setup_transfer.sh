6d3c770a9270bc381e9252ef6146d0d7854c8ccd
dtoya <80666145+dtoya@users.noreply.github.com>
2022-07-03 22:23:23 +0900

    Update omz_setup_transfer.sh

--- omz/setup/omz_setup_transfer.sh
+++ omz/setup/omz_setup_transfer.sh
@@ -1,5 +1,5 @@
 cd ~/handson
 scp -r open_model_zoo/demos/ $TUSER@$TIP:~/handson/omz_demos_src/
-scp -r omz_demos_build/intel64/Release/　$TUSER@$TIP:~/handson/omz_demos_bin
+scp -r omz_demos_build/intel64/Release/ $TUSER@$TIP:~/handson/omz_demos_bin
 scp -r /opt/intel/openvino_2022/extras/opencv/lib $TUSER@$TIP:~/handson/libopencv
 
